b264c29c701e94cf114fa41d4e59bb387f9849a2
Bill Graham Peacemaker <grahambill011@gmail.com>
2025-09-04 22:50:55 +0300

    removing glpi comments

--- src/Agent/Communication/AbstractRequest.php
+++ src/Agent/Communication/AbstractRequest.php
@@ -1,38 +1,5 @@
 <?php
 
-/**
- * ---------------------------------------------------------------------
- *
- * GLPI - Gestionnaire Libre de Parc Informatique
- *
- * http://glpi-project.org
- *
- * @copyright 2015-2025 Teclib' and contributors.
- * @copyright 2003-2014 by the INDEPNET Development Team.
- * @licence   https://www.gnu.org/licenses/gpl-3.0.html
- *
- * ---------------------------------------------------------------------
- *
- * LICENSE
- *
- * This file is part of GLPI.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- * ---------------------------------------------------------------------
- */
-
 namespace Glpi\Agent\Communication;
 
 use DOMDocument;
@@ -41,12 +8,7 @@ use Glpi\Agent\Communication\Headers\Common;
 use Glpi\Application\ErrorHandler;
 use Toolbox;
 
-/**
- * Handle agent requests
- * Both XML (legacy) and JSON inventory formats are supported.
- *
- * @see https://github.com/glpi-project/inventory_format/blob/master/inventory.schema.json
- */
+
 abstract class AbstractRequest
 {
     public const DEFAULT_FREQUENCY = 24;
