3b7b6019fd068b41d2a17d1430b6c9bbda728c71
Bill Graham Peacemaker <grahambill011@gmail.com>
2025-09-04 22:07:14 +0300

    updating index.php, licence header

--- public/index.php
+++ public/index.php
@@ -3,38 +3,37 @@
 /**
  * ---------------------------------------------------------------------
  *
- * GLPI - Gestionnaire Libre de Parc Informatique
+ * Zentra - IT Asset & Service Management System
  *
- * http://glpi-project.org
+ * http://zentra-project.org
  *
- * @copyright 2015-2025 Teclib' and contributors.
- * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @copyright 2025 Zentra and contributors.
  * @licence   https://www.gnu.org/licenses/gpl-3.0.html
  *
  * ---------------------------------------------------------------------
  *
  * LICENSE
  *
- * This file is part of GLPI.
+ * This file is part of Zentra, a customized distribution of GLPI.
  *
- * This program is free software: you can redistribute it and/or modify
+ * Zentra is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Zentra is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  *
  * ---------------------------------------------------------------------
  */
 
 /**
- * GLPI web router.
+ * Zentra web router.
  *
  * This router is used to be able to expose only the `/public` directory on the webserver.
  */
@@ -42,20 +41,12 @@
 $glpi_root  = realpath(dirname(__FILE__, 2));
 
 if (preg_match('/^\/public/', $_SERVER['REQUEST_URI']) !== 1 && $_SERVER['SCRIPT_NAME'] === '/public/index.php') {
-    // When requested URI does not start with '/public' but `$_SERVER['SCRIPT_NAME']` is '/public/index.php',
-    // it means that document root is the GLPI root directory, but a rewrite rule redirects the request to the PHP router.
-    // This case happen when redirection to PHP router is made by an `.htaccess` file placed in the GLPI root directory,
-    // and has to be handled to support shared hosting where it is not possible to change the web server root directory.
     $uri_prefix = '';
 } else {
-    // `$_SERVER['SCRIPT_NAME']` corresponds to the script path relative to server document root.
-    // -> if server document root is `/public`, then `$_SERVER['SCRIPT_NAME']` will be equal to `/index.php`
-    // -> if script is located into a `/glpi-alias` alias directory, then `$_SERVER['SCRIPT_NAME']` will be equal to `/glpi-alias/index.php`
     $uri_prefix = rtrim(str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'])), '/');
 }
 
-// Get URI path relative to GLPI (i.e. without alias directory prefix).
-$path       = preg_replace(
+$path = preg_replace(
     '/^' . preg_quote($uri_prefix, '/') . '/',
     '',
     parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH)
@@ -66,11 +57,7 @@ require $glpi_root . '/src/Http/ProxyRouter.php';
 $proxy = new \Glpi\Http\ProxyRouter($glpi_root, $path);
 
 if ($proxy->isTargetAPhpScript() && $proxy->isPathAllowed() && ($target_file = $proxy->getTargetFile()) !== null) {
-    // Ensure `getcwd()` and inclusion path is based on requested file FS location.
     chdir(dirname($target_file));
-
-    // Redefine some $_SERVER variables to have same values whenever scripts are called directly
-    // or through current router.
     $target_path     = $uri_prefix . $proxy->getTargetPath();
     $target_pathinfo = $proxy->getTargetPathInfo();
     $_SERVER['PATH_INFO']       = $target_pathinfo;
@@ -78,7 +65,6 @@ if ($proxy->isTargetAPhpScript() && $proxy->isPathAllowed() && ($target_file = $
     $_SERVER['SCRIPT_FILENAME'] = $target_file;
     $_SERVER['SCRIPT_NAME']     = $target_path;
 
-    // Execute target script.
     require($target_file);
     exit();
 }
